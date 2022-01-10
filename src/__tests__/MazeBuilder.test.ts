2393fe7ff8dede1e7da66f4f9a9d283ce44b4648
Simon Cragg <simoncragg@icloud.com>
2022-01-10 22:15:37 +0000

    chore: test backtracking in maze builder

--- src/__tests__/MazeBuilder.test.ts
+++ src/__tests__/MazeBuilder.test.ts
@@ -5,7 +5,11 @@ import { Point } from "../Point";
 import { Size } from "../Size";
 
 describe("maze builder", () => {
-  it.only("can build", () => {
+  afterEach(() => {
+    resetMockRandom();
+  });
+
+  it("can build maze (no backtracking required)", () => {
     const mazeBuilder = new MazeBuilder();
     const size = new Size(2, 2);
     const exits = [new Point(1, 0)];
@@ -17,7 +21,8 @@ describe("maze builder", () => {
       [Direction.north, Direction.west],
     ];
 
-    mockRandom([0.5]); // Math.floor(0.5 * 2) === 1
+    const deterministicSequenceForSteppingSouth = [0.5];
+    mockRandom(deterministicSequenceForSteppingSouth);
 
     const maze = mazeBuilder.build(size, exits);
 
@@ -33,7 +38,40 @@ describe("maze builder", () => {
     }
 
     expect(maze.cells[1].position.equals(exits[0])).toBeTruthy();
+  });
 
-    resetMockRandom();
+  it("can build maze (requires backtracking)", () => {
+    const mazeBuilder = new MazeBuilder();
+    const size = new Size(3, 2);
+    const exits = [new Point(2, 0)];
+
+    const expectedCellDirections = [
+      [Direction.east],
+      [Direction.south, Direction.west],
+      [Direction.south],
+      [Direction.east],
+      [Direction.north, Direction.east, Direction.west],
+      [Direction.north, Direction.west],
+    ];
+
+    const deterministicSequenceForSteppingEastThenSouthThenWest = [
+      0.1, 0.5, 0.5,
+    ];
+    mockRandom(deterministicSequenceForSteppingEastThenSouthThenWest);
+
+    const maze = mazeBuilder.build(size, exits);
+
+    for (let y = 0; y < size.height; y++) {
+      for (let x = 0; x < size.width; x++) {
+        const cell = maze.getCell(x, y);
+        const expectedDirections = expectedCellDirections.shift();
+        expect(cell.directions.length).toBe(expectedDirections?.length);
+        for (let i = 0; i < cell.directions.length; i++) {
+          expect(cell.directions[i]).toBe(expectedDirections?.[i]);
+        }
+      }
+    }
+
+    expect(maze.cells[2].position.equals(exits[0])).toBeTruthy();
   });
 });
