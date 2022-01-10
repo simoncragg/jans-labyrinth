b20303b6e5300ae652971b0c42c2fd603db8a89a
Simon Cragg <simoncragg@icloud.com>
2022-01-10 22:16:02 +0000

    chore: test backtracking in maze walker

--- src/__tests__/MazeWalker.test.ts
+++ src/__tests__/MazeWalker.test.ts
@@ -6,12 +6,17 @@ import { Point } from "../Point";
 import { Size } from "../Size";
 
 describe("maze walker", () => {
-  it.only("can find exit", () => {
+  afterEach(() => {
+    resetMockRandom();
+  });
+
+  it("can find exit (no backtracking required)", () => {
     const mazeBuilder = new MazeBuilder();
     const size = new Size(2, 2);
     const exits = [new Point(1, 0)];
 
-    mockRandom([0.5]); // Math.floor(0.5 * 2) === 1
+    const deterministicSequenceToStepSouth = [0.5];
+    mockRandom(deterministicSequenceToStepSouth);
 
     const maze = mazeBuilder.build(size, exits);
     const startPos = new Point(0, 0);
@@ -25,7 +30,31 @@ describe("maze walker", () => {
 
     expect(mazeWalker.currentCell.isExit).toBeTruthy();
     expect(mazeWalker.currentDirection).toBe(Direction.north);
+  });
 
-    resetMockRandom();
+  it("can find exit (requires backtracking and dead-end detection)", () => {
+    const mazeBuilder = new MazeBuilder();
+    const size = new Size(3, 2);
+    const exits = [new Point(2, 0)];
+
+    const deterministicSequenceForSteppingEastThenSouthThenWest = [
+      0.1, 0.5, 0.5,
+    ];
+    mockRandom(deterministicSequenceForSteppingEastThenSouthThenWest);
+
+    const maze = mazeBuilder.build(size, exits);
+    const startPos = new Point(0, 0);
+    const mazeWalker = new MazeWalker(maze, startPos, 0.0);
+
+    const expectedStepsToReachExit = 5;
+    for (let i = 0; i < expectedStepsToReachExit; i++) {
+      const ts = performance.now();
+      mazeWalker.update(ts);
+    }
+
+    expect(mazeWalker.currentCell.isExit).toBeTruthy();
+    expect(mazeWalker.currentDirection).toBe(Direction.north);
+    expect(maze.cells.filter((cell) => cell.isDeadEnd).length).toBe(1);
+    expect(maze.cells[3].isDeadEnd).toBeTruthy();
   });
 });
