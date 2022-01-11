f5da05a4566d5b1e17de6a0c28fe34137255545a
Simon Cragg <simoncragg@icloud.com>
2022-01-11 00:05:00 +0000

    fix: lack of backtracking in maze walker test case

--- src/__tests__/MazeWalker.test.ts
+++ src/__tests__/MazeWalker.test.ts
@@ -15,8 +15,8 @@ describe("maze walker", () => {
     const size = new Size(2, 2);
     const exits = [new Point(1, 0)];
 
-    const deterministicSequenceToStepSouth = [0.5];
-    mockRandom(deterministicSequenceToStepSouth);
+    const deterministicSequenceForSteppingSouth = [0.5];
+    mockRandom(deterministicSequenceForSteppingSouth);
 
     const maze = mazeBuilder.build(size, exits);
     const startPos = new Point(0, 0);
@@ -32,11 +32,40 @@ describe("maze walker", () => {
     expect(mazeWalker.currentDirection).toBe(Direction.north);
   });
 
-  it("can find exit (requires backtracking and dead-end detection)", () => {
+  it("can find exit (with dead-end detection and backtracking)", () => {
+    const mazeBuilder = new MazeBuilder();
+    const size = new Size(4, 3);
+    const exits = [new Point(2, 0)];
+
+    // East, South, East, East, North {East} {Backtrack 2} {South} {West} {West} {West} {North}
+    const deterministicSequenceForStepping = [0.1, 0.5, 0.1, 0.5, 0.1];
+    mockRandom(deterministicSequenceForStepping);
+
+    const maze = mazeBuilder.build(size, exits);
+    const startPos = new Point(0, 0);
+    const mazeWalker = new MazeWalker(maze, startPos, 0.0);
+
+    const deterministicSequenceForSteppingSouth = [0.5];
+    mockRandom(deterministicSequenceForSteppingSouth);
+
+    const expectedStepsToReachExit = 14;
+    for (let i = 0; i < expectedStepsToReachExit; i++) {
+      const ts = performance.now();
+      mazeWalker.update(ts);
+    }
+
+    expect(mazeWalker.currentCell.isExit).toBeTruthy();
+    expect(mazeWalker.currentDirection).toBe(Direction.west);
+    expect(maze.cells.filter((cell) => cell.isDeadEnd).length).toBe(1);
+    expect(maze.cells[4].isDeadEnd).toBeTruthy();
+  });
+
+  it("can find exit (requires dead-end detection)", () => {
     const mazeBuilder = new MazeBuilder();
     const size = new Size(3, 2);
     const exits = [new Point(2, 0)];
 
+    // East, South, West {Backtrack 1} {East} {North}
     const deterministicSequenceForSteppingEastThenSouthThenWest = [
       0.1, 0.5, 0.5,
     ];
